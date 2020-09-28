1e48ce0982d74375623d7e2058b3067adbb7c879
cristianunicam <cristian.verdecchia@studenti.unicam.it>
2020-09-28 18:38:10 +0200

    First commit
    Set up development environment

---
+++ server_utility/server.js
@@ -0,0 +1,21 @@
+const {Client} = require("@googlemaps/google-maps-services-js");
+const client = new Client({});
+
+client
+    .directions(
+        
+    )
+client
+  .elevation({
+    params: {
+      locations: [{ lat: 45, lng: -110 }],
+      key: "AIzaSyB8gWlCkrp_kaqEy-dzRX_JNhLe2NtQjWI",
+    },
+    timeout: 1000, // milliseconds
+  })
+  .then((r) => {
+    console.log(r.data.results[0].elevation);
+  })
+  .catch((e) => {
+    console.log(e.response.data.error_message);
+  });
