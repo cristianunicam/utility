be822478e452afb466e4cec2ad99c4e9e64ab44d
cristianunicam <cristian.verdecchia@studenti.unicam.it>
2020-10-01 12:16:14 +0200

    Added functionality
    Server side now find the data inside the csv file, convert in json and return to the client the requested data given the following path: "%host_address%/path/id"

--- server_utility/structure/route.js
+++ server_utility/structure/route.js
@@ -1,24 +1,25 @@
 const express = require("express");
-const csv = require('csv-parser')
+const csv=require('csv-parser')
+const csvToJson=require('csvtojson')
 const fs = require('fs')
 var gpxParse = require("gpx-parse");
 let route = express();
-var builtPath;
 
-var routeData={};
-
-
+var builtPathGPXFile;
+var routeData;
 const csvFile = "./parse_cai_data/data.csv";
-const results = [];
 
 route
     .route("/:id")
     .get((req, res) => {
         //res.send("GET ID: "+req.params.id);
         builtPath = "./route_gpx/"+req.params.id+".gpx";
-        parseCSV(req.params.id)
+        parseCSV(req.params.id,res)
         //res.send(parseGPX(builtPath));
     });
+
+
+
 
 function parseGPX(idPath){
     console.log("ID PATH: "+idPath);
@@ -33,13 +34,34 @@ function parseGPX(idPath){
         });
 }
 
-function parseCSV(id){
-    fs.createReadStream(csvFile)
+
+
+
+function parseCSV(id,res){
+    csvToJson()
+        .fromFile(csvFile)
+        .then((jsonObj)=>{
+            this.routeData=jsonObj
+            var foundItem = this.routeData.filter(function(item) {
+                return item.id == id;
+            });
+            console.log(foundItem);
+            res.send(foundItem);
+        })
+    /*var data = fs.readFileSync(csvFile)
+    .toString() // convert Buffer to string
+    .split('\n') // split string to lines
+    .map(e => e.trim()) // remove white spaces for each line
+    .map(e => e.split(',').map(e => e.trim())); // split each line to array
+
+    console.log(data);
+    console.log(JSON.stringify(data, '', 2));*/
+    /*fs.createReadStream(csvFile)
         .pipe(csv())
         .on('data', (data) => results.push(data))
         .on('end', () => {
             console.log(results.filter(block => block.id==id));
-        });
+        });*/
 }
 
 module.exports = route;
